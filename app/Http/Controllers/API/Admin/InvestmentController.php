405a597e7e96543049e14b9b667f617758f6fedb
Awofodu Samuel <awofodus@gmail.com>
2020-09-17 14:29:22 +0100

    bonus issues

--- app/Http/Controllers/API/Admin/InvestmentController.php
+++ app/Http/Controllers/API/Admin/InvestmentController.php
@@ -198,10 +198,14 @@ class InvestmentController extends Controller
     {
         if($search = \Request::get('q'))
         {
-            $investments = Investment::where(function ($query) use ($search){
+            $investments = Investment::with('user.referrals.referrer','user.bank',
+                'merges.withdrawal.user.bank','merges.referral_withdrawal.user.bank')
+                ->join('users', 'investments.user_id','=','users.id')
+                ->where(function ($query) use ($search){
                 $query->where('investment_id','LIKE',"%$search%");
-            })->with('user.referrals.referrer','user.bank',
-                'merges.withdrawal.user.bank','merges.referral_withdrawal.user.bank')->latest('investments.created_at')->paginate(1000000);
+                $query->where('users.email','LIKE',"%$search%");
+                $query->where('users.name','LIKE',"%$search%");
+            })->latest('investments.created_at')->paginate(1000000);
             return response()->json(['investments'=>$investments]);
         }
     }
