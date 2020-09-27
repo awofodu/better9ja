be7e5b1f93e399837cb725e6694e16ed45947a05
Awofodu Samuel <awofodus@gmail.com>
2020-09-27 21:19:55 +0100

    cool thing

--- resources/js/components/investor/investment/Create.vue
+++ resources/js/components/investor/investment/Create.vue
@@ -341,7 +341,7 @@
                 axios.get('/api/investor/investments')
                     .then(result=>{
                         this.user = result.data.user;
-                        this.maintenance_fee = parseint(result.data.bonus)+parseInt(result.data.amount) > 4999 ?
+                        this.maintenance_fee = parseInt(result.data.bonus)+parseInt(result.data.amount) > 4999 ?
                             parseInt(result.data.bonus)+parseInt(result.data.amount) * (30/100) :
                             result.data.last_investment ? parseInt(result.data.last_investment.amount) * (10/100) : 0;
                         this.maintenance = result.data.maintenance;
