6fc5683fd5c9086ce538a3ff0bfe51e98bb51323
Awofodu Samuel <awofodus@gmail.com>
2020-10-05 13:03:53 +0100

    semt

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/update', function (){
             }else{
                 $last_investment = Investment::whereUserId($user->id)->latest()->first();
                 $maintenance = $user->maintenances()->latest()->first();
-                $maintenance->charge = (int)$last_investment['amount'] * (10/100);
+                $maintenance->charge = $last_investment ? (int)$last_investment['amount'] * (10/100) : 0;
                 $maintenance->save();
             }
 
